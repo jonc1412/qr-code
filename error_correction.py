dbe76c67023a53f7ceca685b8c650e4209de3f60
jonc1412 <jon.c2436@gmail.com>
2025-07-10 17:52:00 +0900

    split_codewords uses a single dictionary with group as keys and a list that stores each codeword as value

--- error_correction.py
+++ error_correction.py
@@ -1,5 +1,5 @@
 from constants import ERROR_CORRECTION
-from encoder import encode_data
+from encoder import encode_data, get_version
 
 def split_codewords(data, qr_version , err_corr):
     num_blocks_group1 = ERROR_CORRECTION[f'{qr_version}-{err_corr}']["num_blocks_group1"]
@@ -11,37 +11,29 @@ def split_codewords(data, qr_version , err_corr):
     print(f'Number of blocks in group 2: {num_blocks_group2} with {num_codewords_group2} codewords')
     print(f'The total number of codeword blocks: {num_blocks_group1 * num_codewords_group1 + num_blocks_group2 * num_codewords_group2}\n\n')
 
-    group1_dict = dict()
+    split_dict = dict()
+    split_dict[1] = []
 
     codeword_start_index = 0
     codeword_final_index = 8
 
-    for i in range(1, num_blocks_group1+1):
-        group1_dict[i] = dict()
-
-        for j in range(0, num_codewords_group1):
+    for i in range(num_blocks_group1):
+        for j in range(num_codewords_group1):
             current_codeword = data[codeword_start_index:codeword_final_index]
-            group1_dict[i][j] = current_codeword
+            split_dict[1].append(current_codeword)
 
             codeword_start_index += 8
             codeword_final_index += 8
-        
-    print(group1_dict)
 
     if num_blocks_group2 > 0:
-        group2_dict = dict()
+        split_dict[2] = []
 
-
-        for i in range(1, num_blocks_group2+1):
-            group2_dict[i] = []
-            
-            for j in range(0, num_codewords_group2):
+        for i in range(num_blocks_group2):
+            for j in range(num_codewords_group2):
                 current_codeword = data[codeword_start_index:codeword_final_index]
-                group2_dict[i][j] = current_codeword
+                split_dict[2].append(current_codeword)
 
                 codeword_start_index += 8
                 codeword_final_index += 8
 
-        print(group2_dict)
-
-print(split_codewords(encode_data("HELLO WORLD", "Alphanumeric", "M"), 1, "M"))
+    return split_dict
