b63a8b4f0a4d27ba3750e6369ae4df75587f166e
jonc1412 <jon.c2436@gmail.com>
2025-07-10 17:14:09 +0900

    added test function for encode_data

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,4 +1,4 @@
-from encoder import get_version, encode_alphanumeric, encode_byte
+from encoder import get_version, encode_alphanumeric, encode_data
 
 def test_get_version():
     assert get_version("Alphanumeric", len("HELLO WORLD"), "Q") == 1
@@ -7,3 +7,5 @@ def test_get_version():
 def test_encode_alphanumeric():
     assert encode_alphanumeric("HELLO WORLD") == '0110000101101111000110100010111001011011100010011010100001101'
 
+def test_encode_data():
+    assert encode_data("HELLO WORLD", "Alphanumeric", "M") == "00100000010110110000101101111000110100010111001011011100010011010100001101000000111011000001000111101100000100011110110000010001"
