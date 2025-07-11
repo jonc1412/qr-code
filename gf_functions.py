49f3fc0191cebb018082d65581073007dd33cb73
jonc1412 <jon.c2436@gmail.com>
2025-07-11 17:50:40 +0900

    mult and div functions added

--- gf_functions.py
+++ gf_functions.py
@@ -14,7 +14,7 @@ def add_sub(a, b):
 
 exp_table = [0] * 255
 # Important to not access log_table[0] as there is no such value a^i == 0 for some integer i
-log_table = [-1] * 255
+log_table = [-1] * 256
 
 x = 1
 for i in range(255):
@@ -23,6 +23,18 @@ for i in range(255):
     log_table[x] = i
     # Shifting to get the next power of 2
     x <<= 1
-    if x > 255:
+    if x & 0x100:
         # Need to do polynomial modulo (not integer modulo)
         x ^= 0x11D
+
+def mult(a, b):
+    if a == 0 or b == 0:
+        return 0
+    else:
+        return exp_table[(log_table[a] + log_table[b]) % 255]
+    
+def div(a, b):
+    if a == 0 or b == 0:
+        raise ZeroDivisionError()
+    else:
+        return exp_table[(log_table[a] - log_table[b]) % 255]
